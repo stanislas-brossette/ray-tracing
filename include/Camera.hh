619b7e1423ae1786470a858f1a6be119b5c9d496
stanislas-brossette <stanislas.brossette@gmail.com>
2020-03-01 10:00:21 +0100

    Camera fovX fovY

--- include/Camera.hh
+++ include/Camera.hh
@@ -2,17 +2,21 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <cstdlib>
 
 #include "Vector3.hh"
+#include "LightRay.hh"
 
 class Camera
 {
 public:
-  Camera (const Vector3& pos, const Vector3& dir, double fov);
+  Camera (const Vector3& pos, const Vector3& dir, int fovX, int fovY);
   virtual ~Camera ();
   std::string describe() const;
+  LightRay castRandomRay() const;
 
   Vector3 pos_;
   Vector3 dir_;
-  double fov_;
+  int fovX_;
+  int fovY_;
 };
--- src/Camera.cc
+++ src/Camera.cc
@@ -1,14 +1,25 @@
 #include "Camera.hh"
 
-Camera::Camera(const Vector3& pos, const Vector3& dir, double fov)
+Camera::Camera(const Vector3& pos, const Vector3& dir, int fovX, int fovY)
   : pos_(pos),
     dir_(dir),
-    fov_(fov)
+    fovX_(fovX),
+    fovY_(fovY)
 {
 }
 
 Camera::~Camera()
 {
+}
+
+LightRay Camera::castRandomRay() const
+{
+  std::cout << "castRandomRay" << std::endl;
+  double randX = double(std::rand() % fovX_) - double(fovX_) / 2;
+  double randY = double(std::rand() % fovY_) - double(fovY_) / 2;
+  std::cout << "randX: " << randX << std::endl;
+  std::cout << "randY: " << randY << std::endl;
+
 }
 
 std::string Camera::describe() const
@@ -17,6 +28,6 @@ std::string Camera::describe() const
   ss << "=== Camera ===\n";
   ss << "pos: " << pos_ << "\n";
   ss << "dir: " << dir_ << "\n";
-  ss << "fov: " << fov_;
+  ss << "fov: " << fovX_ << ", " << fovY_;
   return ss.str();
 }

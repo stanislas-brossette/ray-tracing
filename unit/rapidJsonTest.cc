e04cb17a16e0741bad1123b597ca82784aea759c
stanislas-brossette <stanislas.brossette@gmail.com>
2020-03-14 11:09:56 +0100

    Scan camera from json

--- unit/rapidJsonTest.cc
+++ unit/rapidJsonTest.cc
@@ -4,8 +4,47 @@
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+#include "Vector3.hh"
 #include <iostream>
 #include <cstdio>
+
+using namespace rapidjson;
+
+struct camData
+{
+    int resX = 0;
+    double fovX = 0;
+    double fovY = 0;
+    Vector3 pos = Vector3(0,0,0);
+    Vector3 rotAxis = Vector3(0,0,0);
+    double rotAngle = 0;
+};
+
+void scanVector3(Value& vIn, Vector3& vRes)
+{
+    std::vector<double> vec3;
+    for (auto& vec : vIn.GetArray())
+        vec3.push_back(vec.GetDouble());
+    vRes = Vector3(vec3[0], vec3[1], vec3[2]);
+}
+
+camData scanCamera(Value& v)
+{
+    camData c;
+    c.resX = v.FindMember("resX")->value.GetInt();
+    c.fovX = v.FindMember("fovX")->value.GetDouble();
+    c.fovY = v.FindMember("fovY")->value.GetDouble();
+    c.rotAngle = v.FindMember("rotAngle")->value.GetDouble();
+    scanVector3(v.FindMember("pos")->value, c.pos);
+    scanVector3(v.FindMember("rotAxis")->value, c.rotAxis);
+    std::cout << "c.resX: " << c.resX << std::endl;
+    std::cout << "c.fovX: " << c.fovX << std::endl;
+    std::cout << "c.fovY: " << c.fovY << std::endl;
+    std::cout << "c.rotAxis: " << c.rotAxis << std::endl;
+    std::cout << "c.rotAngle: " << c.rotAngle << std::endl;
+    std::cout << "c.pos: " << c.pos << std::endl;
+    return c;
+}
  
 TEST(rapidJsonTest, RapidJsonTest)
 {
@@ -13,32 +52,59 @@ TEST(rapidJsonTest, RapidJsonTest)
     FILE* fp = fopen(path.c_str(), "r");
  
     char readBuffer[65536];
-    rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+    FileReadStream is(fp, readBuffer, sizeof(readBuffer));
     std::cout << "readBuffer: " << readBuffer << std::endl;
      
-    rapidjson::Document d;
-    d.Parse(readBuffer);
-    std::cout << "d.IsObject(): " << d.IsObject() << std::endl;
+    Document document;
+    document.Parse(readBuffer);
+    std::cout << "document.IsObject(): " << document.IsObject() << std::endl;
     fclose(fp);
 
-    if(d.HasParseError())
+    if(document.HasParseError())
     {
         std::cout << "ERROR: Json has errors" << std::endl;
     }
 
-    rapidjson::Value::MemberIterator sceneIt;
-    for(sceneIt = d.MemberBegin(); sceneIt != d.MemberEnd(); sceneIt++)
+    static const char* kTypeNames[] =
+        { "Null", "False", "True", "Object", "Array", "String", "Number" };
+    for (Value::ConstMemberIterator itr = document.MemberBegin();
+        itr != document.MemberEnd(); ++itr)
     {
-        std::string sceneName(sceneIt->name.GetString());
-        std::cout << "sceneName: " << sceneName << std::endl;
-
-        rapidjson::Value::MemberIterator elementIt;
-        for(elementIt = d[sceneName].MemberBegin(); elementIt != d[sceneName].MemberEnd(); elementIt++)
+        std::string name(itr->name.GetString());
+        camData cam;
+        if(name == "camera")
         {
-            std::string elementName(elementIt->name.GetString());
-            std::cout << "elementName: " << elementName << std::endl;
+            cam = scanCamera(document[itr->name.GetString()]);
+        }
+        printf("Type of member %s is %s\n",
+            itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+        if(kTypeNames[itr->value.GetType()] == "Object")
+        {
+            std::cout << "This is an object" << std::endl;
+            Value& obj = document[name.c_str()];
+            for (Value::ConstMemberIterator itr2 = obj.MemberBegin();
+                itr2 != obj.MemberEnd(); ++itr2)
+            {
+                printf("Type of member %s is %s\n",
+                    itr2->name.GetString(), kTypeNames[itr2->value.GetType()]);
+            }
         }
     }
+
+
+    //Value::MemberIterator sceneIt;
+    //for(sceneIt = document.MemberBegin(); sceneIt != document.MemberEnd(); sceneIt++)
+    //{
+    //    std::string sceneName(sceneIt->name.GetString());
+    //    std::cout << "sceneName: " << sceneName << std::endl;
+
+    //    Value::MemberIterator elementIt;
+    //    for(elementIt = document[sceneName].MemberBegin(); elementIt != document[sceneName].MemberEnd(); elementIt++)
+    //    {
+    //        std::string elementName(elementIt->name.GetString());
+    //        std::cout << "elementName: " << elementName << std::endl;
+    //    }
+    //}
      
     ASSERT_TRUE(true);
 }

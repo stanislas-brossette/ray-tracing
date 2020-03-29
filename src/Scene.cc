2ed197fa053c0642d2a25f4c486fb9d3bbd66243
stanislas-brossette <stanislas.brossette@wandercraft.eu>
2020-03-29 10:49:23 +0200

    misc

--- src/Scene.cc
+++ src/Scene.cc
@@ -241,9 +241,9 @@ double Scene::getDistReductionFactor(double dist) const
 {
     //double distReductionFactor = 1/std::sqrt(dist+1);
     //double distReductionFactor = 1/(dist+1);
-    //double distReductionFactor = 1/std::pow(dist+1,2);
+    double distReductionFactor = 3.14/std::pow(dist+1,2);
     //double distReductionFactor = 1/std::pow(dist/4+1,3);
-    double distReductionFactor = 1;
+    //double distReductionFactor = 1;
     return distReductionFactor;
 }
 
@@ -574,7 +574,7 @@ void Scene::castRay(Pixel& pix, const LightRay& lr, size_t depthIndex) const
         pix += specReflPix;
         pix += diffuseRefPix;
 
-        //pix.applyGammaCorrection(1.0, 2.2);
+        pix.applyGammaCorrection(1.0, 2.2);
         pix.clamp();
     }
 }
